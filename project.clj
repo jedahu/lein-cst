6bf7ef8fc854ad12df55c0597b61a869cfed8943
Jeremy Hughes <jedahu@gmail.com>
2012-04-21 14:47:02 +1200

    use lein 2 and lein-sub

--- project.clj
+++ project.clj
@@ -6,7 +6,14 @@
   [[org.clojure/clojure "1.3.0"]
    [fs "1.1.2"]]
 
+  :plugins
+  [[lein-sub "0.2.0"]]
+
   :exclusions
   [org.apache.ant/ant]
 
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :sub ["cst"]
+
+  :min-lein-version "2.0.0")
