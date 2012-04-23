0680803ef6414c7db9306535f0b61bda19de992e
Jeremy Hughes <jedahu@gmail.com>
2012-04-23 14:43:56 +1200

    next snapshot version

--- samples/hello/project.clj
+++ samples/hello/project.clj
@@ -1,7 +1,7 @@
 (defproject
   hello "0.1.0-SNAPSHOT"
   :description "helloworld clojurescript"
-  :plugins [[lein-cst "0.2.2"]]
+  :plugins [[lein-cst "0.2.2-SNAPSHOT"]]
   ; :dependencies [[org.clojure/clojure "1.3.0"]]
   ; :dev-dependencies [[cljs-tools "0.1.0-SNAPSHOT"]]
   :extra-classpath-dirs ["src"]
